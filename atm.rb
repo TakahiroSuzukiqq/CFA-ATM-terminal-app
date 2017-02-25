164f280a73b6a343068c9a2f5adf3d0bff384c60
TakahiroSuzukiqq <takahirosuzuki.k010@icloud.com>
2017-02-25 22:28:21 +1100

    Update atm.rb

--- atm.rb
+++ atm.rb
@@ -1,76 +1,40 @@
-#I have a pen
-
-
-# @balance = 100
-#
-# def withdraw
-#    @balance = @balance - 20
-#   puts("Current balance is now $#{@balance}")
-# end
-#
-# def deposit
-#    @balance = @balance + 20
-#    puts("Current balance is now $#{@balance}")
-# end
-#
-#
-#   puts "Do you want to withdraw or deposit 20 from your bank account"
-#
-# 10.times do
-#
-#   puts "Enter (1) to withdraw $20"
-#   puts "Enter (2) to deposit $20"
-#   choice = gets.chomp
-#   if choice == "1"
-#      withdraw
-#   elsif choice =="2"
-#      deposit
-#   end
-#
-# end
-
-# @balance = 100
-# def withdraw
-#     @balance
-
-
-
-
+ #atm-challenge
 #Set up the initial balance
-balance = 100
+###############################################
+current_balance = 100
 
-def deposit
-   balance = balance + 20
-end
-
-#Loop through 10times (This should be a until statement)
-puts "hey, waht do you want to do"
-puts "YOU have $#{balance}"
-puts "1. Withdraw"
-puts "2. Deposit"
-puts "3. add20"
-
-#get the user input
-answer =gets.chomp#,to_i
-puts answer
-
-#Based on what user haa input to something
-#Withdraw
- if answer ==1
+def withdraw(balance)
    balance = balance - 20
-   puts balance
-
-#Deposit
- elsif answer ==2
-   balance = balance + 20
-   puts balance
-
-#add20
- elsif answer ==3
-   balance = balance + 20
-   puts balance
-
- else
-  puts "wtf did you type"
- end
 end
+
+def deposit(balance)
+   balance = balance + 20
+end
+
+def name_answer(name_answer)
+  name_answer = name_answer
+end
+###############################################
+puts "Hello, what's your name?"
+name_answer = gets.chomp
+puts "Hello #{name_answer}"
+
+10.times do
+  puts "#{name_answer},your current balance is $#{current_balance}"
+  puts "What do you want to do ?"
+  puts "Enter (1) to withdraw $20"
+  puts "Enter (2) to deposit $20"
+  choice = gets.chomp
+
+
+  if choice == "1"
+     current_balance = withdraw(current_balance)
+     puts "Your current balance is $#{current_balance}."
+  elsif choice =="2"
+     current_balance = deposit(current_balance)
+     puts "Your current balance is $#{current_balance}."
+  else
+    puts "Error, try again."
+         end
+  end
+  ###############################################
